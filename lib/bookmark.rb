6b6af37e6148d15bebb53bc0925feb58080600d3
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-07-16 17:38:41 +0100

    step7

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,18 +1,9 @@
+require 'pg'
+
 class Bookmark
-
     def self.all
-        [
-            "http://www.makersacademy.com",
-            "http://www.destroyallsoftware.com",
-            "http://www.google.com"
-        ]
+      connection = PG.connect(dbname: 'bookmark_manager')
+      result = connection.exec('SELECT * FROM bookmarks')
+      result.map { |bookmark| bookmark['url'] }
     end
-
-    # def initialize(bookmarks_array=[])
-    #     @bookmarks_array = bookmarks_array
-    # end
-
-    # def all
-    #     @bookmarks_array
-    # end
-end
+  end
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -1,11 +1,13 @@
 require 'bookmark'
 
 describe Bookmark do
-    it 'returns a list of bookmarks' do
-        expect(Bookmark.all).to eq [
-            "http://www.makersacademy.com",
-            "http://www.destroyallsoftware.com",
-            "http://www.google.com"
-        ]
-    end
+    describe '.all' do
+        it 'returns a list of bookmarks' do
+          bookmarks = Bookmark.all
+      
+          expect(bookmarks).to include "http://www.makersacademy.com"
+          expect(bookmarks).to include "http://www.destroyallsoftware.com"
+          expect(bookmarks).to include "http://www.google.com"
+        end
+      end
 end
